f7e1dffc00e726393bfc18c018605d04be2c4c28
damiandrabek <damiandrabek1703@gmail.com>
2025-10-20 17:46:25 +0200

    continuing to develop stock details

--- app/stocks/[id].tsx
+++ app/stocks/[id].tsx
@@ -4,33 +4,46 @@ import { useLocalSearchParams } from 'expo-router'
 
 import { fetchStockDetails } from "@/services/api";
 import useFetch from "@/services/useFetch";
-import { ID } from 'react-native-appwrite';
 
 const StockDetails = () => {
 
   const { id } = useLocalSearchParams();
 
-  const fetchFn = useCallback(() => fetchStockDetails(id.toString()), []);
+  const fetchFn = useCallback(() => fetchStockDetails(id as string), []);
   const {
     data: stock,
     loading: stockLoading,
     error: stockError,
-  } = useFetch(fetchFn, true);
+  } = useFetch<Stock>(fetchFn, true);
 
 
   return (
     <View className="bg-primary flex-1">
       <ScrollView contentContainerStyle={{ paddingBottom: 80 }}>
         <View>
-          <Image source={{ uri: stock.logo }} />
-          <Text className="text-white">{id}</Text>
-          <Text className="text-white">{stock?.currentPrice.toFixed(2)}</Text>
+          <Image 
+            source={{ uri: stock?.logo 
+              ? stock?.logo 
+              : "https://placehold.co/600x400/1a1a1a/ffffff?text=Not+Found",
+            }}
+            className='w-12 h-[120px]'
+            resizeMode='stretch' />
+          
         </View>
+
+        <View className="flex-col items-start mt-5 px-5">
+          <Text className="text-white font-bold text-xl">{stock?.ticker}</Text>
+
+          <View className='flex-row items-center gap-x-1 mt-2'>
+            <Text className='text-light 200 font-sm'>${stock?.name}</Text>
+            <Text className='text-light 200 font-sm'>${stock?.currentPrice}</Text>
+
+          </View>
+        </View>
+
       </ScrollView>
     </View>
   );
 };
 
 export default StockDetails
-
-const styles = StyleSheet.create({})
